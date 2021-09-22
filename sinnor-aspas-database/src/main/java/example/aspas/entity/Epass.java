14e985d8362c38b36978ada4ecc2cf13140ec5a9
anettaj <58222128+anettaj@users.noreply.github.com>
2021-09-22 21:55:19 +0530

    Update Epass.java

--- sinnor-aspas-database/src/main/java/example/aspas/entity/Epass.java
+++ sinnor-aspas-database/src/main/java/example/aspas/entity/Epass.java
@@ -20,6 +20,40 @@ public class Epass {
 	private Date date_To;
 	// defining Date_To as column name
 	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public Date getDate_To() {
+		return date_To;
+	}
+	public void setDate_To(Date date_To) {
+		this.date_To = date_To;
+	}
+	@Override
+	public String toString() {
+		return "Epass [status=" + status + ", dateFrom=" + dateFrom + ", date_To=" + date_To + "]";
+	}
+	public Epass(String status, Date dateFrom, Date date_To) {
+		super();
+		this.status = status;
+		this.dateFrom = dateFrom;
+		this.date_To = date_To;
+	}
+	public Epass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 
 }
